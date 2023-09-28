34d54cad4cbf4b04070d0c9124be8d24500566d1
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-09-28 19:55:24 +0530

    🎨 Format backorder.util.util

--- backorder/util/util.py
+++ backorder/util/util.py
@@ -1,113 +1,66 @@
 import os
-from sys import exc_info
+from pathlib import Path
 
 import dill
 import numpy as np
 import pandas as pd
 import yaml
 
-from backorder.constant import *
-from backorder.exception import BackorderException
+from backorder import constant as C
 
 
-def write_yaml_file(file_path: str, data: dict = None):
-    """
-    Create yaml file
-    file_path: str
-    data: dict
-    """
-    try:
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, "w") as yaml_file:
-            if data is not None:
-                yaml.dump(data, yaml_file)
-    except Exception as e:
-        raise BackorderException(e, exc_info())
+def write_yaml_file(file_path: Path, data: dict | None = None):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    with open(file_path, "w") as yaml_file:
+        if data is not None:
+            yaml.dump(data, yaml_file)
 
 
-def read_yaml_file(file_path: str) -> dict:
-    """
-    Reads a YAML file and returns the contents as a dictionary.
-    file_path: str
-    """
-    try:
-        with open(file_path, "rb") as yaml_file:
-            return yaml.safe_load(yaml_file)
-    except Exception as e:
-        raise BackorderException(e, exc_info()) from e
+def read_yaml_file(file_path: Path) -> dict:
+    with open(file_path, "rb") as yaml_file:
+        return yaml.safe_load(yaml_file)
 
 
-def save_numpy_array_data(file_path: str, array: np.array):
-    """
-    Save numpy array data to file
-    file_path: str location of file to save
-    array: np.array data to save
-    """
-    try:
-        dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, "wb") as file_obj:
-            np.save(file_obj, array)
-    except Exception as e:
-        raise BackorderException(e, exc_info()) from e
+def save_numpy_array_data(file_path: Path, array: np.ndarray):
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
+
+    with open(file_path, "wb") as file_obj:
+        np.save(file_obj, array)
 
 
-def load_numpy_array_data(file_path: str) -> np.array:
-    """
-    load numpy array data from file
-    file_path: str location of file to load
-    return: np.array data loaded
-    """
-    try:
-        with open(file_path, "rb") as file_obj:
-            return np.load(file_obj, allow_pickle=True)
-    except Exception as e:
-        raise BackorderException(e, exc_info()) from e
+def load_numpy_array_data(file_path: Path) -> np.ndarray:
+    with open(file_path, "rb") as file_obj:
+        return np.load(file_obj, allow_pickle=True)
 
 
-def save_object(file_path: str, obj):
-    """
-    file_path: str
-    obj: Any sort of object
-    """
-    try:
-        dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
-    except Exception as e:
-        raise BackorderException(e, exc_info()) from e
+def save_object(file_path: Path, obj):
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
+
+    with open(file_path, "wb") as file_obj:
+        dill.dump(obj, file_obj)
 
 
-def load_object(file_path: str):
-    """
-    file_path: str
-    """
-    try:
-        with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
-    except Exception as e:
-        raise BackorderException(e, exc_info()) from e
+def load_object(file_path: Path):
+    with open(file_path, "rb") as file_obj:
+        return dill.load(file_obj)
 
 
-def load_data(file_path: str, schema_file_path: str) -> pd.DataFrame:
-    try:
-        datatset_schema = read_yaml_file(schema_file_path)
+def load_data(file_path: Path, schema_file_path: Path) -> pd.DataFrame:
+    dataset_schema = read_yaml_file(schema_file_path)
+    schema = dataset_schema[C.DATASET_SCHEMA_COLUMNS_KEY]
+    dataframe = pd.read_csv(file_path)
 
-        schema = datatset_schema[DATASET_SCHEMA_COLUMNS_KEY]
+    not_available_cols = []
+    for column in dataframe.columns:
+        if column in list(schema.keys()):
+            dataframe[column].astype(schema[column])
+        else:
+            not_available_cols.append(column)
 
-        dataframe = pd.read_csv(file_path)
+    if len(not_available_cols) > 0:
+        raise ValueError(f"{not_available_cols!r} not present in the dataframe.")
 
-        error_messgae = ""
-
-        for column in dataframe.columns:
-            if column in list(schema.keys()):
-                dataframe[column].astype(schema[column])
-            else:
-                error_messgae = f"{error_messgae} \nColumn: [{column}] is not in the schema."
-        if len(error_messgae) > 0:
-            raise Exception(error_messgae)
-        return dataframe
-
-    except Exception as e:
-        raise BackorderException(e, exc_info()) from e
+    return dataframe
